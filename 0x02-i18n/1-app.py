4a2a4b9f82a72c8692f7665165d38de1b1e321bb
T-sosthenese <sosthenetimi94@gmail.com>
2024-01-09 16:21:47 +0300

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+A simple flask app using flask-babel
+"""
+from flask_babel import Babel
+from flask import Flask, render_template
+
+
+class Config:
+    """
+    Represents a flask babel configuration
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@app.route('/')
+def get_index() -> str:
+    """
+    Renders the home page
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
